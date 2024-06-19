e6e8f93c3d272a8c0f496433e4b7d1fc915d59a7
quadrey-sheggzie <akanniquadrey@gmail.com>
2024-06-19 15:14:04 +0100

    added buttons and clicks function

--- app.py
+++ app.py
@@ -3,4 +3,18 @@ import tkinter as tk
 root = tk.Tk()
 root.title("Calculator")
 
+def showClick():
+    print(display.get())
+
+display = tk.Entry(root)
+display.grid(row=0, column=0, padx=20, pady=20)
+
+btn = tk.Button(root, text="click me!", command=showClick)
+btn1 = tk.Button(root, text="click me!", command=showClick)
+btn2 = tk.Button(root, text="click me!", command=showClick)
+
+btn.grid(row=1, column=0, padx=20, pady=20)
+btn1.grid(row=1, column=1, padx=20, pady=20)
+btn2.grid(row=1, column=2, padx=20, pady=20)
+
 root.mainloop()
